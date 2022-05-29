833c55a9065953b8a241a814e5487f3c4f846afa
Cyril-Meyer <contact@cyrilmeyer.eu>
2022-05-29 04:08:23 +0200

    add view_users_list.py script

---
+++ view_users_list.py
@@ -0,0 +1,14 @@
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser(description='view user list')
+parser.add_argument('filename', type=str, help='user filename')
+args = parser.parse_args()
+filename = args.filename
+
+db = np.load(filename)
+assert db.dtype == np.int64
+assert len(db.shape) == 1
+
+for user in db:
+    print(user)
