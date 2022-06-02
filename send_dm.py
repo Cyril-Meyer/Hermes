2abcecf198ed1f2470212869074c4e93ab41bd1a
Cyril-Meyer <contact@cyrilmeyer.eu>
2022-06-03 00:25:42 +0200

    add timer option to send_dm

--- send_dm.py
+++ send_dm.py
@@ -7,10 +7,12 @@ parser = argparse.ArgumentParser(description='get user list')
 parser.add_argument('token', type=str, help='discord token')
 parser.add_argument('filename', type=str, help='user filename')
 parser.add_argument('message', type=str, help='message filename')
+parser.add_argument('--timer', type=int, default=1, help='time to wait (minutes) between messages')
 args = parser.parse_args()
 token = args.token
 filename = args.filename
 message = args.message
+timer = args.timer
 
 members_id = np.load(filename)
 message = open(message, 'r', encoding='utf-8')
@@ -37,7 +39,7 @@ async def on_ready():
         user = await client.fetch_user(member_id)
         result = await user.send(message)
         print(member_id, user, 'ERROR' if result.flags.value else 'OK')
-        time.sleep(1.0)
+        time.sleep(timer)
     print('----------------------------------------')
     await client.close()
 
